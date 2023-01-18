5859ebf32d636bd8c308738e590411ad1504ca97
juanclbautista <juanc.lopezbautista@gmail.com>
2023-01-17 18:11:57 -0600

    Form Wave Animation

---
+++ Day 8/script.js
@@ -0,0 +1,11 @@
+const labels = document.querySelectorAll('.form-control label')
+
+labels.forEach((label) => {
+  label.innerHTML = label.innerText
+    .split('')
+    .map(
+      (leter, idx) =>
+        `<span style="transition-delay:${idx * 50}ms">${letter}</span>`
+    )
+    .join('')
+})
